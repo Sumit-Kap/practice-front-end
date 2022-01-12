f39fdcd194e7a90e9c432a77b5b0de7a452e8051
Sumit Kapoor <sumwin@Sumits-MacBook-Air.local>
2022-01-12 20:38:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+export default function App() {
+  const [response, setResponse] = React.useState([]);
+  const fetchData = async (id) => {
+    const response = await fetch(`http://localhost:4000/movies?id=${id}`);
+    const responseData = await response.json();
+    setResponse([...responseData.data]);
+  };
+  console.log(response);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <button
+          onClick={() => {
+            fetchData(1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          1
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            fetchData(2);
+          }}
+        >
+          2
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            fetchData(3);
+          }}
+        >
+          3
+        </button>
+      </div>
+      {response.length > 0 && (
+        <div>
+          <h1>{response[0].name}</h1>
+          <h2>{response[0].rating}</h2>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
